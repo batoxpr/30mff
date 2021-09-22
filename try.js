0cb9f0ef6afaefac5604830eda4006480360f071
batoxpr <90750426+batoxpr@users.noreply.github.com>
2021-09-22 21:22:38 +0400

    Add files via upload

---
+++ try.js
@@ -0,0 +1,13 @@
+
+console.log("Hello JS");
+
+
+var cute;
+cute = "Hello World;
+cute = cute + "What u up to";
+
+var myObject = {};
+var myObject = {
+  anObjectProp = 10;
+  anotherOne = "Hi bae";
+}
